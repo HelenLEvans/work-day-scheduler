1a3de1511211243de42ab15c14aed25b02d56968
Fifinella <h.hoesley@gmail.com>
2022-11-20 13:28:14 -0800

    added HTML time blocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,75 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the HTML.
+$(document).ready(() => {
+ 
+});
+
+let saveBtn = $(".saveBtn")
+
+//Display current time
+$("#currentDay").text(dayjs().format("dddd MMMM DD YYYY"));
+
+// function displayTimeTop() {
+//     $("#currentDay").text(moment().format("dddd MMMM Do YYYY hh:mm:ss A"));
+
+
+//TODO: wrapping function to ensure code doesn't run until browser renders
+// $(function () {
+
+//     //variables
+//     let saveBtn = $(".saveBtn");
+
+    //functions
+    function colorBlock() {
+        let timeNow = dayjs().hour();
+        // console.log(timeNow);
+
+        $(".time-schedule").each(function () {
+            let timeSchedule = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (timeSchedule < scheduleHour) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeSchedule === scheduleHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    //TODO: click event listener using 'this'
+    saveBtn.on("click", function () {
+        let time = $(this).siblings(".hour").text();
+        let plan = $(this).siblings(".plan").val();
+        let toDoList = $('#toDo');
+        localStorage.setItem(time, plan);
+        $('#toDoHeader').show();
+        toDoList.append('<li>' + time + " " + plan + '</li>');
+    });
+
+    function usePlanner() {
+        //TODO: setting value of local storage
+        $(".hour").each(function () {
+            let currHour = $(this).text();
+            let currPlan = localStorage.getItem(currHour);
+
+            if (currPlan !== null) {
+                $(this).siblings(".plan").val(currPlan);
+            }
+        });
+    }
+
+    colorBlock();
+    usePlanner();
+
+
+
